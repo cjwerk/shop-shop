e4d80b2f30236eea1ae5464d26d3d231c2f62438
Chris Werkheiser <cjwerk@gmail.com>
2021-03-13 18:58:51 -0800

    refactoring

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,51 +1,51 @@
-import React, { useEffect } from "react";
-import { useQuery } from "@apollo/client";
-import { useStoreContext } from "../../utils/GlobalState";
-import {
-  UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY,
-} from "../../utils/actions";
+import React, { useEffect } from 'react';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
+import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from "../../utils/queries";
-import { idbPromise } from "../../utils/helpers";
+// import { useStoreContext } from "../../utils/GlobalState";
+import { idbPromise } from '../../utils/helpers';
 import { useSelector, useDispatch } from "react-redux";
 
 function CategoryMenu() {
-  const state = useSelector((state) => state);
-  const dispatch = useDispatch();
-  const { categories } = state;
+  // const [state, dispatch] = useStoreContext();
+  const state = useSelector(state => state);
+
+  const dispatch = useDispatch(); 
 
   const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+  const { categories } = state;
 
   useEffect(() => {
     if (categoryData) {
       dispatch({
         type: UPDATE_CATEGORIES,
-        categories: categoryData.categories,
+        categories: categoryData.categories
       });
-      categoryData.categories.forEach((category) => {
-        idbPromise("categories", "put", category);
+      categoryData.categories.forEach(category => {
+        idbPromise('categories', 'put', category);
       });
     } else if (!loading) {
-      idbPromise("categories", "get").then((categories) => {
+      idbPromise('categories', 'get').then(categories => {
         dispatch({
           type: UPDATE_CATEGORIES,
-          categories: categories,
+          categories: categories
         });
       });
     }
   }, [categoryData, loading, dispatch]);
 
-  const handleClick = (id) => {
+  const handleClick = id => {
     dispatch({
       type: UPDATE_CURRENT_CATEGORY,
-      currentCategory: id,
+      currentCategory: id
     });
   };
 
   return (
     <div>
       <h2>Choose a Category:</h2>
-      {categories.map((item) => (
+      {categories.map(item => (
         <button
           key={item._id}
           onClick={() => {
--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
-import { useMutation } from '@apollo/client';
 import Jumbotron from "../components/Jumbotron";
-import { ADD_ORDER } from "../utils/mutations";
+import { useMutation } from "@apollo/client";
+import { ADD_ORDER } from "../utils/mutations.js";
 import { idbPromise } from "../utils/helpers";
 
 function Success() {
@@ -9,39 +9,35 @@ function Success() {
 
   useEffect(() => {
     async function saveOrder() {
-      const cart = await idbPromise('cart', 'get');
-      const products = cart.map(item => item._id);
-      
+      const cart = await idbPromise("cart", "get");
+      const products = cart.map((item) => item._id);
+
       if (products.length) {
         const { data } = await addOrder({ variables: { products } });
         const productData = data.addOrder.products;
-    
+
         productData.forEach((item) => {
-          idbPromise('cart', 'delete', item);
+          idbPromise("cart", "delete", item);
         });
       }
-        
-      setTimeout(() => {
-        window.location.assign('/');
-      }, 3000);
     }
 
     saveOrder();
   }, [addOrder]);
 
+  setTimeout(function () {
+    window.location.assign("/");
+  }, 3000);
+
   return (
     <div>
       <Jumbotron>
         <h1>Success!</h1>
-        <h2>
-          Thank you for your purchase!
-        </h2>
-        <h2>
-          You will now be redirected to the home page
-        </h2>
+        <h2>Thank you for your purchase!</h2>
+        <h2>You will now be redirected to the homepage</h2>
       </Jumbotron>
     </div>
   );
-};
+}
 
 export default Success;
